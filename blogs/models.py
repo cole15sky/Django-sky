cbc968f67c00d165e43b825193886accc3844754
Sky Blue <dawadiaakash@gmail.com>
2024-12-14 16:25:06 +0545

    chore: update models and migrate

--- blogs/models.py
+++ blogs/models.py
@@ -4,9 +4,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
      user =  models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
      title = models.CharField(max_length=255)
-     description = models.TextField(null=True,blank=True)
      created = models.DateTimeField(auto_now_add=True)
      body = models.TextField()
      
      def __str__(self):
-         return self.title + ' ' + self.author
+         return self.title + ' | ' +  str(self.user)
